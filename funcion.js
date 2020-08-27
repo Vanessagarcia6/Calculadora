2594355655206b7a85013afd18c8dd5a78b9109e
Vanessagarcia6 <69133029+Vanessagarcia6@users.noreply.github.com>
2020-08-27 15:20:39 -0500

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,119 @@
+//Declaramos variables globales osea que se puede utilizar en cualquier parte del codigo dependiento lo que se necesite 
+//o es hacer llamado de ellas
+var operandoa;
+var operandob;
+var operacion;
+
+
+
+function init() { //inicio de funcion
+
+    var resultado = document.getElementById('resultado');
+    var reset = document.getElementById('reset');
+    var suma = document.getElementById('suma');
+    var resta = document.getElementById('resta');
+    var multiplicacion = document.getElementById('multiplicacion');
+    var division = document.getElementById('division');
+    var igual = document.getElementById('igual');
+    var uno = document.getElementById('uno');
+    var dos = document.getElementById('dos');
+    var tres = document.getElementById('tres');
+    var cuatro = document.getElementById('cuatro');
+    var cinco = document.getElementById('cinco');
+    var seis = document.getElementById('seis');
+    var siete = document.getElementById('siete');
+    var ocho = document.getElementById('ocho');
+    var nueve = document.getElementById('nueve');
+    var cero = document.getElementById('cero');
+
+    //significa que cuando el usuario de en el boton uno el va a buscar ese boton y lo relaciona con lo que tiene en comillas
+    // y la e es paraq guardarlo momentaneamente
+    uno.onclick = function(e) {
+        resultado.textContent = resultado.textContent + "1";
+    }
+    dos.onclick = function(e) {
+        resultado.textContent = resultado.textContent + "2";
+    }
+    tres.onclick = function(e) {
+        resultado.textContent = resultado.textContent + "3";
+    }
+    cuatro.onclick = function(e) {
+        resultado.textContent = resultado.textContent + "4";
+    }
+    cinco.onclick = function(e) {
+        resultado.textContent = resultado.textContent + "5";
+    }
+    seis.onclick = function(e) {
+        resultado.textContent = resultado.textContent + "6";
+    }
+    siete.onclick = function(e) {
+        resultado.textContent = resultado.textContent + "7";
+    }
+    ocho.onclick = function(e) {
+        resultado.textContent = resultado.textContent + "8";
+    }
+    nueve.onclick = function(e) {
+        resultado.textContent = resultado.textContent + "9";
+    }
+    cero.onclick = function(e) {
+        resultado.textContent = resultado.textContent + "0";
+    }
+    reset.onclick = function(e) {
+        resetear();
+    }
+    suma.onclick = function(e) {
+        operandoa = resultado.textContent;
+        operacion = "+";
+        limpiar();
+    }
+    resta.onclick = function(e) {
+        operandoa = resultado.textContent;
+        operacion = "-";
+        limpiar();
+    }
+    multiplicacion.onclick = function(e) {
+        operandoa = resultado.textContent;
+        operacion = "*";
+        limpiar();
+    }
+    division.onclick = function(e) {
+        operandoa = resultado.textContent;
+        operacion = "/";
+        limpiar();
+    }
+    igual.onclick = function(e) {
+        operandob = resultado.textContent;
+        resolver();
+    }
+} //termina la funcion
+
+function limpiar() {
+    resultado.textContent = "";
+}
+
+function resetear() {
+    resultado.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+}
+
+function resolver() {
+    var res = 0;
+    switch (operacion) {
+        case "+":
+            res = parseFloat(operandoa) + parseFloat(operandob);
+            break;
+        case "-":
+            res = parseFloat(operandoa) - parseFloat(operandob);
+            break;
+        case "*":
+            res = parseFloat(operandoa) * parseFloat(operandob);
+            break;
+        case "/":
+            res = parseFloat(operandoa) / parseFloat(operandob);
+            break;
+    }
+    resetear();
+    resultado.textContent = res;
+}
